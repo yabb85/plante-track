d512bdac2c2e90d49019cf296d5bc182161aa6e2
Laurent Hory <laurent.hory@gmail.com>
2016-07-06 22:12:12 +0200

    add logger

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,23 @@
 #! /usr/bin/python
 # -*- coding:utf-8 -*-
 
+from logging import DEBUG
+from logging import getLogger
 from flask import Flask
+from logging.handlers import RotatingFileHandler
 
 """
 Initialisation package
 """
 app = Flask(__name__)
 app.secret_key = 'plante website'
+
+handler = RotatingFileHandler('ueki.log', maxBytes=10000, backupCount=1)
+handler.setLevel(DEBUG)
+app.logger.addHandler(handler)
+
+logger = getLogger('werkzeug')
+logger.addHandler(handler)
 
 sensors = {}
 
